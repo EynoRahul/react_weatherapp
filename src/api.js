25dacaa149f08b8cf4369c98530b220afd84ebfb
Rahul Sharma <rahulsharmabhumca16@gmail.com>
2022-08-21 02:51:20 +0530

    added first widget till 1:12:00 done...:)

--- src/api.js
+++ src/api.js
@@ -6,4 +6,8 @@ export const geoApioptions = {
 	}
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "25945d6a5d3292b455c0dc7cdbb570a4";
